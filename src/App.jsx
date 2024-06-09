d179081c1f610bf15e1f723b6a2c240f7cf08a33
OmeletteX <chen.qixuan97@gmail.com>
2024-06-10 00:29:04 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Page1 from "./components/Page1.jsx";
+import Page2 from "./components/Page2.jsx";
+
+
+export default function App() {
+    return (
+        <>
+            <Page1 />
+            <Page2 />
+
+
+        </>
+    )
+}
+
+
